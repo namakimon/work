f2959b5bcb0d0c6681e767b1e5eb78bfd0cf475a
Denis <batusai_65@mail.ru>
2018-11-23 15:18:36 +0300

    Это наш первый комит

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function myFirstApp(name, age) {
+
+	alert("Привет, меня зовут "+name+" и это моя первая программа!");
+
+	function showSkills() {
+		let skills = ['html', 'css', 'jQuery'];
+
+		for (let i = 0; i < skills.length; i++) {
+			document.write("Я владею " + skills[i] + "<br>");
+		}
+	}
+
+	showSkills();
+
+	function checkAge() {
+		if (age > 18){
+			alert("Вы нам идеально подходите!");
+		} else {
+			alert("Приходи через годик, другой!");
+		}
+	}
+
+	function calcPow(num) {
+		console.log( num*num );
+	}
+
+	calcPow(4);
+
+	checkAge();
+
+}
+	myFirstApp("Ivan", 32);
